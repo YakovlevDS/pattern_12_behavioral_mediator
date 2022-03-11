2952d723fa425828f5430271ad8226379c349511
Dmitriy Yakovlev <1d3ddd@gmail.com>
2022-03-11 18:08:53 +0200

    feat:add example

--- mediator.js
+++ mediator.js
@@ -9,7 +9,7 @@ class OfficialDealer {
     console.log(`Additional info: ${info}`);
     this.addToCustomersList(name);
   }
-  
+  h
   addToCustomersList(name) {
     this.customers.push(name);
   }
@@ -33,3 +33,11 @@ class Customer {
     this.dealerMediator.orderAuto(this, auto, info)
   }
 };
+const mediator = new OfficialDealer()
+const dzhon =new Customer('Dzhon',mediator)
+const valya =new Customer('Valya',mediator)
+
+dzhon.makeOrder('Audi','With parktronik!');
+valya.makeOrder('Tesla','With parktronik!');
+console.log(mediator.getCustomerList());
+
